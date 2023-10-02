26cc7b831dc7dd0611403c1a48d95796399543bb
sfomuseumbot <sfomuseumbot@localhost>
2023-10-02 11:00:25 -0700

    snapshot: convert CIImage to Data; compiles but untested

--- Sources/ImageEmbossGRPC/Embosser.swift
+++ Sources/ImageEmbossGRPC/Embosser.swift
@@ -48,12 +48,26 @@ final class ImageEmbosser: EmbosserAsyncProvider {
          switch rsp {
          case .failure(_):
              throw(Errors.processError)
-         case .success(let im):
+         case .success(let im_rsp):
+             
+             var data = [Data]()
+             
+             for im in im_rsp {
+                 
+                 let png_rsp = CoreImageImage.AsPNGData(image: im)
+                 
+                 switch png_rsp {
+                 case .failure(let png_err):
+                     throw(png_err)
+                 case .success(let png_data):
+                     data.append(png_data)
+                 }
+             }
              
              return EmbossImageResponse.with{
                  $0.filename = request.filename
                  // To do: Return a repeatable blob of bytes
-                 // $0.body = Data(im)
+                 $0.body = data
                  $0.combined = request.combined
              }
              
