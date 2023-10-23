c68c14cd946a0e0efa6254fc888dec92e8f5450f
sfomuseumbot <sfomuseumbot@localhost>
2023-10-23 15:15:57 -0700

    move all the CLI logging stuff in to image-emboss-grpc-server/Logger.swift

--- Scripts/image-emboss-grpc-server/Server.swift
+++ Scripts/image-emboss-grpc-server/Server.swift
@@ -1,26 +1,6 @@
 import ArgumentParser
-import Logging
 import ImageEmbossGRPC
 import GRPCServer
-import Puppy
-import Foundation
-
-struct LogFormatter: LogFormattable {
-    private let dateFormat = DateFormatter()
-
-    init() {
-        dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-    }
-
-    func formatMessage(_ level: LogLevel, message: String, tag: String, function: String,
-                       file: String, line: UInt, swiftLogInfo: [String : String],
-                       label: String, date: Date, threadID: UInt64) -> String {
-        let date = dateFormatter(date, withFormatter: dateFormat)
-        let fileName = fileName(file)
-        let moduleName = moduleName(file)
-        return "\(date) \(threadID) [\(level)] \(swiftLogInfo) \(moduleName)/\(fileName)#L.\(line) \(function) \(message)"
-    }
-}
 
 @available(macOS 14.0, iOS 17.0, tvOS 17.0, *)
 @main
@@ -49,49 +29,13 @@ struct ImageEmbossServer: AsyncParsableCommand {
     
   func run() async throws {
 
-    let log_label = "org.sfomuseum.image-emboss-grpc-server"
-    let log_format = LogFormatter()
+      let log_label = "org.sfomuseum.text-emboss-grpc-server"
       
-      // This does not work (yet) as advertised. Specifically only
-      // the first handler added to puppy ever gets invoked. Dunno...
-      
-      var puppy = Puppy()
-
-      if log_file != nil {
-          
-          let log_url = URL(fileURLWithPath: log_file!).absoluteURL
-          
-          let rotationConfig = RotationConfig(suffixExtension: .numbering,
-                                              maxFileSize: 30 * 1024 * 1024,
-                                              maxArchivedFilesCount: 5)
-          
-          let fileRotation = try FileRotationLogger(log_label,
-                                                    logFormat: log_format,
-                                                    fileURL: log_url,
-                                                    rotationConfig: rotationConfig
-          )
-          
-          puppy.add(fileRotation)
-      }
-      
-      // See notes above
-      
-      let console = ConsoleLogger(log_label, logFormat: log_format)
-      puppy.add(console)
-      
-      LoggingSystem.bootstrap {
-          
-          var handler = PuppyLogHandler(label: $0, puppy: puppy)
-          handler.logLevel = .info
-          
-          if verbose {
-              handler.logLevel = .trace
-          }
-          
-          return handler
-      }
-      
-      let logger = Logger(label: log_label)
+      let logger = try NewLogger(
+        log_label: log_label,
+        log_file: log_file,
+        verbose: verbose
+      )
       
       let embosser = NewImageEmbosser(
         logger: logger
