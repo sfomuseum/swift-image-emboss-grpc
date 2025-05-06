3707c335ea7ed11101c12a52152e750e6d6c6429
thisisaaronland <thisisaaronland@localhost>
2025-05-06 10:16:45 -0700

    start swift-grpc-2 refactor, ugh

---
+++ Sources/ImageEmbossGRPC/Serve.swift
@@ -0,0 +1,81 @@
+import Foundation
+import ImageEmboss
+import CoreImage
+import CoreImageImage
+
+struct Server: ImageEmbosser_Server.SimpleServiceProtocol {
+    
+    func embossImage(
+        
+        request: ImageEmbosser_EmbossImageRequest,
+        context: ServerContext
+        
+    ) async throws -> ImageEmbosser_EmbossImageResponse {
+        
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
+                                        isDirectory: true)
+        
+        let temporaryFilename = ProcessInfo().globallyUniqueString
+        
+        let temporaryFileURL =
+        temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
+        
+        try request.body.write(to: temporaryFileURL,
+                               options: .atomic)
+        
+        defer {
+            do {
+                try FileManager.default.removeItem(at: temporaryFileURL)
+            } catch {
+                // self.logger.error("Failed to remove temporary file at \(temporaryFileURL), \(error)")
+            }
+            
+        }
+        
+        var ci_im: CIImage
+        
+        let im_rsp = CoreImageImage.LoadFromURL(url: temporaryFileURL)
+        
+        switch im_rsp {
+        case .failure(let error):
+            // self.logger.error("Failed to load image from \(temporaryFileURL), \(error)")
+            throw(error)
+        case .success(let im):
+            ci_im = im
+        }
+        
+        let te = ImageEmboss()
+        let rsp = te.ProcessImage(image: ci_im, combined: request.combined)
+        
+        switch rsp {
+        case .failure(let error):
+            // self.logger.error("Failed to process image from \(temporaryFileURL), \(error)")
+            throw(Errors.processError)
+        case .success(let im_rsp):
+            
+            var data = [Data]()
+            
+            for im in im_rsp {
+                
+                let png_rsp = CoreImageImage.AsPNGData(image: im)
+                
+                switch png_rsp {
+                case .failure(let png_err):
+                    // self.logger.error("Failed to derive PNG from from \(temporaryFileURL) (processed), \(png_err)")
+                    throw(png_err)
+                case .success(let png_data):
+                    data.append(png_data)
+                }
+            }
+            
+            // self.logger.info("Successfully processed \(temporaryFileURL)")
+            
+            return ImageEmbosser_EmbossImageResponse.with{
+                $0.filename = request.filename
+                $0.body = data
+                $0.combined = request.combined
+            }
+            
+        }
+        
+    }
