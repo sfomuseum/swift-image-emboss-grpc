bc923b208831680febb2d0a83d0887e9919ee4ba
thisisaaronland <thisisaaronland@localhost>
2025-05-06 12:41:21 -0700

    snapshot: builds but still untested

--- Package.swift
+++ Package.swift
@@ -8,16 +8,13 @@ let package = Package(
     platforms: [.macOS(.v15)],
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
-           .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
-           .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0"),
-        
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-        // .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
-
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
         .package(url: "https://github.com/sfomuseum/swift-image-emboss", from: "0.0.5"),
         .package(url: "https://github.com/sfomuseum/swift-coreimage-image.git", from: "1.1.0"),
-        // .package(url: "https://github.com/sfomuseum/swift-sfomuseum-logger.git", from: "1.0.1"),
-        // .package(path: "/usr/local/sfomuseum/swift-sfomuseum-logger")
+        .package(url: "https://github.com/sfomuseum/swift-sfomuseum-logger.git", from: "1.0.1"),
     ],
     targets: [
         .executableTarget(
@@ -27,8 +24,10 @@ let package = Package(
                 .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
                 .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Logging", package: "swift-log"),
                 .product(name: "ImageEmboss", package: "swift-image-emboss"),
                 .product(name: "CoreImageImage", package: "swift-coreimage-image"),
+                .product(name: "SFOMuseumLogger", package: "swift-sfomuseum-logger"),
             ],
             plugins: [
               .plugin(name: "GRPCProtobufGenerator", package: "grpc-swift-protobuf")
--- Sources/ImageEmbosser/Subcommands/Serve.swift
+++ Sources/ImageEmbosser/Subcommands/Serve.swift
@@ -1,5 +1,6 @@
 import ArgumentParser
 import Foundation
+import Logging
 import GRPCCore
 import GRPCNIOTransportHTTP2
 import GRPCProtobuf
@@ -8,99 +9,133 @@ import Synchronization
 import ImageEmboss
 import CoreImage
 import CoreImageImage
+import SFOMuseumLogger
 
 struct Serve: AsyncParsableCommand {
-  static let configuration = CommandConfiguration(abstract: "Starts a route-guide server.")
-
-  @Option(help: "The port to listen on")
-  var port: Int = 8080
-
-  func run() async throws {
-    let transport = HTTP2ServerTransport.Posix(
-      address: .ipv4(host: "127.0.0.1", port: self.port),
-      transportSecurity: .plaintext
-    )
-
-    let server = GRPCServer(transport: transport, services: [ImageEmbosserService()])
-      
-    try await withThrowingDiscardingTaskGroup { group in
-      group.addTask { try await server.serve() }
-      let address = try await transport.listeningAddress
-      print("server listening on \(address)")
+    static let configuration = CommandConfiguration(abstract: "Starts a image embosser server.")
+    
+    @Option(help: "The host name to listen for new connections")
+    var host: String = "localhost"
+    
+    @Option(help: "The port to listen on")
+    var port: Int = 8080
+    
+    @Option(help: "Log events to system log files")
+    var logfile: Bool = false
+    
+    @Option(help: "Enable verbose logging")
+    var verbose: Bool = false
+    
+    @Option(help: "Sets the maximum message size in bytes the server may receive. If 0 then the swift-grpc defaults will be used.")
+    var max_receive_message_length = 0
+    
+    func run() async throws {
+        
+        let log_label = "org.sfomuseum.image-emboss-grpc-server"
+        
+        let logger_opts = SFOMuseumLoggerOptions(
+            label: log_label,
+            console: true,
+            logfile: logfile,
+            verbose: verbose
+        )
+        
+        let logger = try NewSFOMuseumLogger(logger_opts)
+        
+        let service = ImageEmbosserService(logger: logger)
+        
+        let transport = HTTP2ServerTransport.Posix(
+            address: .ipv4(host: self.host, port: self.port),
+            transportSecurity: .plaintext
+        )
+        
+        
+        let server = GRPCServer(transport: transport, services: [service])
+        
+        try await withThrowingDiscardingTaskGroup { group in
+            group.addTask { try await server.serve() }
+            let address = try await transport.listeningAddress
+            print("server listening on \(address)")
+        }
     }
-  }
 }
 
 struct ImageEmbosserService: ImageEmbosser_ImageEmbosser.SimpleServiceProtocol {
     
+    let logger: Logger
+    
+    init(logger: Logger) {
+        self.logger = logger
+    }
+    
     func embossImage(request: ImageEmbosser_EmbossImageRequest, context: GRPCCore.ServerContext) async throws -> ImageEmbosser_EmbossImageResponse {
-         
-         let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
-                                         isDirectory: true)
-         
-         let temporaryFilename = ProcessInfo().globallyUniqueString
-         
-         let temporaryFileURL =
-         temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
-         
-         try request.body.write(to: temporaryFileURL,
-                                options: .atomic)
-         
-         defer {
-             do {
-                 try FileManager.default.removeItem(at: temporaryFileURL)
-             } catch {
-                 // self.logger.error("Failed to remove temporary file at \(temporaryFileURL), \(error)")
-             }
-             
-         }
-         
-         var ci_im: CIImage
-         
-         let im_rsp = CoreImageImage.LoadFromURL(url: temporaryFileURL)
-         
-         switch im_rsp {
-         case .failure(let error):
-             // self.logger.error("Failed to load image from \(temporaryFileURL), \(error)")
-             throw(error)
-         case .success(let im):
-             ci_im = im
-         }
-         
-         let te = ImageEmboss()
-         let rsp = te.ProcessImage(image: ci_im, combined: request.combined)
-         
-         switch rsp {
-         case .failure(let error):
-             // self.logger.error("Failed to process image from \(temporaryFileURL), \(error)")
-             throw(error)
-         case .success(let im_rsp):
-             
-             var data = [Data]()
-             
-             for im in im_rsp {
-                 
-                 let png_rsp = CoreImageImage.AsPNGData(image: im)
-                 
-                 switch png_rsp {
-                 case .failure(let png_err):
-                     // self.logger.error("Failed to derive PNG from from \(temporaryFileURL) (processed), \(png_err)")
-                     throw(png_err)
-                 case .success(let png_data):
-                     data.append(png_data)
-                 }
-             }
-             
-             // self.logger.info("Successfully processed \(temporaryFileURL)")
-             
-             return ImageEmbosser_EmbossImageResponse.with{
-                 $0.filename = request.filename
-                 $0.body = data
-                 $0.combined = request.combined
-             }
-             
-         }
-         
-     }
- }
+        
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
+                                        isDirectory: true)
+        
+        let temporaryFilename = ProcessInfo().globallyUniqueString
+        
+        let temporaryFileURL =
+        temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
+        
+        try request.body.write(to: temporaryFileURL,
+                               options: .atomic)
+        
+        defer {
+            do {
+                try FileManager.default.removeItem(at: temporaryFileURL)
+            } catch {
+                // self.logger.error("Failed to remove temporary file at \(temporaryFileURL), \(error)")
+            }
+            
+        }
+        
+        var ci_im: CIImage
+        
+        let im_rsp = CoreImageImage.LoadFromURL(url: temporaryFileURL)
+        
+        switch im_rsp {
+        case .failure(let error):
+            // self.logger.error("Failed to load image from \(temporaryFileURL), \(error)")
+            throw(error)
+        case .success(let im):
+            ci_im = im
+        }
+        
+        let te = ImageEmboss()
+        let rsp = te.ProcessImage(image: ci_im, combined: request.combined)
+        
+        switch rsp {
+        case .failure(let error):
+            // self.logger.error("Failed to process image from \(temporaryFileURL), \(error)")
+            throw(error)
+        case .success(let im_rsp):
+            
+            var data = [Data]()
+            
+            for im in im_rsp {
+                
+                let png_rsp = CoreImageImage.AsPNGData(image: im)
+                
+                switch png_rsp {
+                case .failure(let png_err):
+                    // self.logger.error("Failed to derive PNG from from \(temporaryFileURL) (processed), \(png_err)")
+                    throw(png_err)
+                case .success(let png_data):
+                    data.append(png_data)
+                }
+            }
+            
+            // self.logger.info("Successfully processed \(temporaryFileURL)")
+            
+            return ImageEmbosser_EmbossImageResponse.with{
+                $0.filename = request.filename
+                $0.body = data
+                $0.combined = request.combined
+            }
+            
+        }
+        
+    }
+}
 
